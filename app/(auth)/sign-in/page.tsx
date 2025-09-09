53513ddfe546b738d3560b81589ef30ee86139e3
weily10 <w3ily10@gmail.com>
2025-09-09 17:42:29 +0800

    add form, signin page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import AuthForm from "@/components/AuthForm"
+
+
+const SignIn = () => {
+  
+    return (
+      <section className="flex-center size-full max-sm:px-6">
+         <AuthForm  type="sign-in"></AuthForm>
+      </section>
+    )
+  }
+  
+  
+  export default SignIn
